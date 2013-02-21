d0e207f09fda41c761ff820b9d922e8e60f5bb05
Miguel Vicente Linares <miviclin@gmail.com>
2013-02-21 17:33:17 +0100

    Add Pool and Poolable

---
+++ src/com/miviclin/droidengine2d/util/Poolable.java
@@ -0,0 +1,16 @@
+package com.miviclin.droidengine2d.util;
+
+/**
+ * Interfaz que utiliza Pool para asignar los valores por defecto a las propiedades de los objetos que almacena.
+ * 
+ * @author Miguel Vicente Linares
+ * 
+ */
+public interface Poolable {
+	
+	/**
+	 * Reinicia las propiedades del objeto a los valores por defecto
+	 */
+	public void reset();
+	
+}
