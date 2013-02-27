80d7ad2bd52425e881fd1832b1f945f1fbc85f94
Miguel Vicente Linares <miviclin@gmail.com>
2013-02-27 18:40:39 +0100

    Add javadoc to GameView

--- src/com/miviclin/droidengine2d/ui/GameView.java
+++ src/com/miviclin/droidengine2d/ui/GameView.java
@@ -13,16 +13,46 @@ import android.view.View.OnTouchListener;
  */
 public interface GameView {
 	
+	/**
+	 * Devuelve el ancho del GameView
+	 * 
+	 * @return ancho
+	 */
 	public int getWidth();
 	
+	/**
+	 * Devuelve el alto del GameView
+	 * 
+	 * @return alto
+	 */
 	public int getHeight();
 	
+	/**
+	 * Asigna un OnClickListener al GameView
+	 * 
+	 * @param onClickListener OnClickListener a asignar. Si es null, se eliminara el OnClickListener que hubiera.
+	 */
 	public void setOnClickListener(OnClickListener onClickListener);
 	
+	/**
+	 * Asigna un OnLongClickListener al GameView
+	 * 
+	 * @param onLongClickListener OnLongClickListener a asignar. Si es null, se eliminara el OnClickListener que hubiera.
+	 */
 	public void setOnLongClickListener(OnLongClickListener onLongClickListener);
 	
+	/**
+	 * Asigna un OnKeyListener al GameView
+	 * 
+	 * @param onKeyListener OnKeyListener a asignar. Si es null, se eliminara el OnClickListener que hubiera.
+	 */
 	public void setOnKeyListener(OnKeyListener onKeyListener);
 	
+	/**
+	 * Asigna un OnTouchListener al GameView
+	 * 
+	 * @param onTouchListener OnTouchListener a asignar. Si es null, se eliminara el OnClickListener que hubiera.
+	 */
 	public void setOnTouchListener(OnTouchListener onTouchListener);
 	
 }
