32da18c85644bc8deddfba02f4957540ebe9bfa7
Miguel Vicente Linares <miviclin@gmail.com>
2013-03-01 18:15:07 +0100

    Add *.util.time with TimeCounter and TimeConstants
    - TimeCounter is a time counter.
    
    - TimeConstants contains a set of constants that ease conversion between
      different time units.

---
+++ src/com/miviclin/droidengine2d/util/time/TimeConstants.java
@@ -0,0 +1,17 @@
+package com.miviclin.droidengine2d.util.time;
+
+/**
+ * Constantes de tiempo
+ * 
+ * @author Miguel Vicente Linares
+ * 
+ */
+public class TimeConstants {
+	
+	public static final int NANOSECONDS_PER_SECOND = 1000000000;
+	public static final int MILLISECONDS_PER_SECOND = 1000;
+	public static final int SECONDS_PER_MINUTE = 60;
+	public static final int MINUTES_PER_HOUR = 60;
+	public static final int HOURS_PER_DAY = 24;
+	
+}
