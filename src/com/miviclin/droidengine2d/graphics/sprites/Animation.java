36634796ef82755532a78af0612be67ecc33212b
Miguel Vicente Linares <miviclin@gmail.com>
2013-03-04 16:20:07 +0200

    Add animation states and javadoc to Animation and AnimationFrame

--- src/com/miviclin/droidengine2d/graphics/sprites/Animation.java
+++ src/com/miviclin/droidengine2d/graphics/sprites/Animation.java
@@ -2,75 +2,179 @@ package com.miviclin.droidengine2d.graphics.sprites;
 
 import java.util.ArrayList;
 
+/**
+ * Animacion
+ * 
+ * @author Miguel Vicente Linares
+ * 
+ */
 public class Animation {
+	
+	private enum State {
+		RUNNING,
+		PAUSED,
+		FINISHED;
+	}
 	
 	private ArrayList<AnimationFrame> frames;
 	private int currentFrameIndex;
 	private int elapsedTime;
-	private boolean repeat;
-	private boolean updateCompleted;
+	private boolean repetitionEnabled;
+	private State state;
 	
+	/**
+	 * Crea una animacion con capacidad inicial de 10 frames y con repeticion activada, de forma que cuando se complete el primer ciclo,
+	 * volvera a repetirse todo el ciclo de la animacion.
+	 */
 	public Animation() {
 		this(10, true);
 	}
 	
+	/**
+	 * Crea una animacion con capaidad inicial para 10 frames.
+	 * 
+	 * @param repeat Indica si se repetira el ciclo de la animacion o no
+	 */
 	public Animation(boolean repeat) {
 		this(10, repeat);
 	}
 	
+	/**
+	 * Crea una animacion
+	 * 
+	 * @param initialCapacity Numero minimo de frames que se pueden agregar antes de que se redimensione la estructura de datos
+	 * @param repeat Indica si se repetira el ciclo de la animacion o no
+	 */
 	public Animation(int initialCapacity, boolean repeat) {
 		this.frames = new ArrayList<AnimationFrame>(initialCapacity);
 		this.currentFrameIndex = 0;
 		this.elapsedTime = 0;
-		this.repeat = repeat;
-		this.updateCompleted = false;
+		this.repetitionEnabled = repeat;
+		this.state = State.RUNNING;
 	}
 	
+	/**
+	 * Agrega un frame a la animacion
+	 * 
+	 * @param frame Nuevo frame, se agregara al final de la lista de frames
+	 */
 	public void addFrame(AnimationFrame frame) {
 		frames.add(frame);
 	}
 	
+	/**
+	 * Actualiza la animacion
+	 * 
+	 * @param delta Tiempo transcurrido desde la ultima actualizacion
+	 */
 	public void update(float delta) {
-		if (frames.size() > 1) {
+		if ((state == State.RUNNING) && (frames.size() > 1)) {
 			elapsedTime += delta;
-			if (elapsedTime > frames.get(currentFrameIndex).getTime()) {
+			if (elapsedTime > frames.get(currentFrameIndex).getDelay()) {
 				elapsedTime = 0;
 				currentFrameIndex++;
 				if (currentFrameIndex >= frames.size()) {
 					currentFrameIndex = 0;
-				}
-			}
-		}
-	}
-	
-	public boolean updateNoRepeat(float delta) {
-		if (!updateCompleted) {
-			if (frames.size() > 1) {
-				elapsedTime += delta;
-				if (elapsedTime > frames.get(currentFrameIndex).getTime()) {
-					elapsedTime = 0;
-					currentFrameIndex++;
-					if (currentFrameIndex >= frames.size()) {
-						currentFrameIndex = 0;
-						updateCompleted = true;
+					if (!repetitionEnabled) {
+						state = State.FINISHED;
 					}
 				}
 			}
 		}
-		return updateCompleted;
 	}
 	
+	/**
+	 * Reinicia la animacion. No vacia la lista de frames.
+	 */
 	public void reset() {
 		currentFrameIndex = 0;
 		elapsedTime = 0;
-		updateCompleted = false;
+		state = State.RUNNING;
 	}
 	
+	/**
+	 * Vacia la lista de frames y reinicia la configuracion
+	 */
+	public void clear() {
+		currentFrameIndex = 0;
+		elapsedTime = 0;
+		state = State.RUNNING;
+		frames.clear();
+	}
+	
+	/**
+	 * Devuelve el frame seleccionado actualmente
+	 * 
+	 * @return AnimationFrame
+	 */
 	public AnimationFrame getCurrentFrame() {
-		if (currentFrameIndex < frames.size()) {
-			return frames.get(currentFrameIndex);
-		}
-		return null;
+		return frames.get(currentFrameIndex);
 	}
 	
+	/**
+	 * Devuelve si la repeticion esta activada
+	 * 
+	 * @return true si se repetite el ciclo de la animacion, false en caso contrario
+	 */
+	public boolean isRepetitionEnabled() {
+		return repetitionEnabled;
+	}
+	
+	/**
+	 * Asigna si se repite el ciclo de la animacion o no
+	 * 
+	 * @param repetitionEnabled Indica si se repetira el ciclo de la animacion o no
+	 */
+	public void setRepetitionEnabled(boolean repetitionEnabled) {
+		this.repetitionEnabled = repetitionEnabled;
+		if (state == State.FINISHED) {
+			state = State.RUNNING;
+		}
+	}
+	
+	/**
+	 * Si la animacion no ha terminado, la pausa. Se puede reanudar con {@link #resume()}
+	 */
+	public void pause() {
+		if (state != State.FINISHED) {
+			state = State.PAUSED;
+		}
+	}
+	
+	/**
+	 * Si la animacion esta pausada, la reanuda. Se puede reanudar con {@link #pause()}
+	 */
+	public void resume() {
+		if (state == State.PAUSED) {
+			state = State.RUNNING;
+		}
+	}
+	
+	/**
+	 * Devuelve si la animacion se esta ejecutando (esta pausada ni ha terminado)
+	 * 
+	 * @return true si la animacion esta ejecutandose, false en caso contrario
+	 */
+	public boolean isRunning() {
+		return state == State.RUNNING;
+	}
+	
+	/**
+	 * Devuelve si la animacion esta pausada
+	 * 
+	 * @return true si la animacion esta pausada, false en caso contrario
+	 */
+	public boolean isPaused() {
+		return state == State.PAUSED;
+	}
+	
+	/**
+	 * Devuelve si la animacion ha terminado.<br>
+	 * Si la repeticion esta activada, devolvera siempre true, puesto que la animacion no termina nunca.
+	 * 
+	 * @return true si la animacion ha terminado, false en caso contrario
+	 */
+	public boolean isFinished() {
+		return state == State.FINISHED;
+	}
 }
--- src/com/miviclin/droidengine2d/graphics/sprites/AnimationFrame.java
+++ src/com/miviclin/droidengine2d/graphics/sprites/AnimationFrame.java
@@ -2,29 +2,61 @@ package com.miviclin.droidengine2d.graphics.sprites;
 
 import com.miviclin.droidengine2d.graphics.textures.TextureRegion;
 
+/**
+ * Representa un frame de animacion
+ * 
+ * @author Miguel Vicente Linares
+ * 
+ */
 public class AnimationFrame {
 	
-	private int time;
+	private int delay;
 	private TextureRegion textureRegion;
 	
-	public AnimationFrame(int time, TextureRegion textureRegion) {
+	/**
+	 * Crea un AnimationFrame
+	 * 
+	 * @param delay Tiempo que se mostrara este frame antes de actualizar al siguiente
+	 * @param textureRegion TextureRegion que se mostrara en este frame
+	 */
+	public AnimationFrame(int delay, TextureRegion textureRegion) {
 		super();
-		this.time = time;
+		this.delay = delay;
 		this.textureRegion = textureRegion;
 	}
 	
-	public int getTime() {
-		return time;
+	/**
+	 * Devuelve el tiempo que se mostrara este frame antes de actualizar al siguiente
+	 * 
+	 * @return tiempo que se mostrara este frame antes de actualizar al siguiente
+	 */
+	public int getDelay() {
+		return delay;
 	}
 	
-	public void setTime(int time) {
-		this.time = time;
+	/**
+	 * Asigna el tiempo que se mostrara este frame antes de actualizar al siguiente
+	 * 
+	 * @param delay Nuevo valor
+	 */
+	public void setDelay(int delay) {
+		this.delay = delay;
 	}
 	
+	/**
+	 * Devuelve el TextureRegion que se mostrara en este frame
+	 * 
+	 * @return TextureRegion que se mostrara en este frame
+	 */
 	public TextureRegion getTextureRegion() {
 		return textureRegion;
 	}
 	
+	/**
+	 * Asigna el TextureRegion que se mostrara en este frame
+	 * 
+	 * @param textureRegion Nuevo TextureRegion
+	 */
 	public void setTextureRegion(TextureRegion textureRegion) {
 		this.textureRegion = textureRegion;
 	}
