0518512a7d5523a848c2d9b080325935fbb973c8
Miguel Vicente Linares <miviclin@gmail.com>
2013-03-31 22:50:59 +0200

    Activate back face culling in DefaultRenderer

--- src/com/miviclin/droidengine2d/graphics/DefaultRenderer.java
+++ src/com/miviclin/droidengine2d/graphics/DefaultRenderer.java
@@ -46,6 +46,7 @@ public class DefaultRenderer implements EngineRenderer {
 		game.getTextureManager().loadAllTextures();
 		
 		GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		GLES20.glEnable(GLES20.GL_CULL_FACE);
 		GLES20.glEnable(GLES20.GL_BLEND);
 		GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
 		
