c021fd6ad4b42fa37d7ee168c4cfed927426f94e
Miguel Vicente Linares <miviclin@gmail.com>
2013-05-21 23:59:08 +0200

    Add PrimitiveTypeSize
    PrimitiveTypeSize contains some constants with the number of bytes that
    most of the primitive types use in java.

---
+++ src/com/miviclin/droidengine2d/util/PrimitiveTypeSize.java
@@ -0,0 +1,13 @@
+package com.miviclin.droidengine2d.util;
+
+public final class PrimitiveTypeSize {
+	
+	public final static int SIZE_OF_BYTE = 1;
+	public final static int SIZE_OF_CHAR = 2;
+	public final static int SIZE_OF_SHORT = 2;
+	public final static int SIZE_OF_INT = 4;
+	public final static int SIZE_OF_LONG = 8;
+	public final static int SIZE_OF_FLOAT = 4;
+	public final static int SIZE_OF_DOUBLE = 8;
+	
+}
