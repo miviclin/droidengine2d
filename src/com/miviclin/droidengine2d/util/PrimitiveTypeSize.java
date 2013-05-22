27118bca64d15822ab9ea80c1812ee63cc1ff288
Miguel Vicente Linares <miviclin@gmail.com>
2013-05-22 09:53:03 +0100

    Add javadoc to PrimitiveTypeSize

--- src/com/miviclin/droidengine2d/util/PrimitiveTypeSize.java
+++ src/com/miviclin/droidengine2d/util/PrimitiveTypeSize.java
@@ -1,13 +1,46 @@
 package com.miviclin.droidengine2d.util;
 
+/**
+ * Clase que contiene constantes que definen el numero de bytes de cada tipo base
+ * 
+ * @author Miguel Vicente Linares
+ * 
+ */
 public final class PrimitiveTypeSize {
 	
+	/**
+	 * Numero de bytes que ocupa un byte
+	 */
 	public final static int SIZE_OF_BYTE = 1;
+	
+	/**
+	 * Numero de bytes que ocupa un char
+	 */
 	public final static int SIZE_OF_CHAR = 2;
+	
+	/**
+	 * Numero de bytes que ocupa un short
+	 */
 	public final static int SIZE_OF_SHORT = 2;
+	
+	/**
+	 * Numero de bytes que ocupa un int
+	 */
 	public final static int SIZE_OF_INT = 4;
+	
+	/**
+	 * Numero de bytes que ocupa un long
+	 */
 	public final static int SIZE_OF_LONG = 8;
+	
+	/**
+	 * Numero de bytes que ocupa un float
+	 */
 	public final static int SIZE_OF_FLOAT = 4;
+	
+	/**
+	 * Numero de bytes que ocupa un double
+	 */
 	public final static int SIZE_OF_DOUBLE = 8;
 	
 }
