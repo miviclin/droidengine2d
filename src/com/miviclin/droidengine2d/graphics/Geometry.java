cbe2317e7ad2605ad682c57d216f52164d903136
Miguel Vicente Linares <miviclin@gmail.com>
2013-05-22 11:40:21 +0200

    Add javadoc to Geometry

--- src/com/miviclin/droidengine2d/graphics/Geometry.java
+++ src/com/miviclin/droidengine2d/graphics/Geometry.java
@@ -4,6 +4,12 @@ import com.miviclin.droidengine2d.util.Array;
 import com.miviclin.droidengine2d.util.math.Vector2;
 import com.miviclin.droidengine2d.util.math.Vector3;
 
+/**
+ * Clase que define la geometria de un objeto grafico: vertices e informacion de los vertices
+ * 
+ * @author Miguel Vicente Linares
+ * 
+ */
 public class Geometry {
 	
 	private Array<Vector3> vertices;
@@ -15,6 +21,11 @@ public class Geometry {
 	private boolean updateTexturesUV;
 	private boolean updateFaces;
 	
+	/**
+	 * Crea un Geometry
+	 * 
+	 * @param builder Builder que se utilizara para crear el objeto
+	 */
 	private Geometry(GeometryBuilder builder) {
 		this.vertices = builder.vertices;
 		this.faces = builder.faces;
@@ -26,26 +37,59 @@ public class Geometry {
 		this.updateTexturesUV = false;
 	}
 	
+	/**
+	 * Agrega un vertice
+	 * 
+	 * @param position Posicion del vertice
+	 */
 	public void addVertex(Vector3 position) {
 		vertices.add(position);
 	}
 	
+	/**
+	 * Devuelve el vertice situado en el indice especificado del Array de vertices
+	 * 
+	 * @param index Indice del vertice
+	 * @return Coordenadas del vertice
+	 */
 	public Vector3 getVertex(int index) {
 		return vertices.get(index);
 	}
 	
+	/**
+	 * Agrega una face a la geometria (indices de los vertices que definen la face)
+	 * 
+	 * @param face Indices de los vertices que definen la face
+	 */
 	public void addFace(Vector3 face) {
 		faces.add(face);
 	}
 	
+	/**
+	 * Devuelve la face situada en el indice especificado del Array de faces
+	 * 
+	 * @param index Indice de la face
+	 * @return Indices que definen la face
+	 */
 	public Vector3 getFace(int index) {
 		return faces.get(index);
 	}
 	
+	/**
+	 * Agrega un color a la geometria. El color ira asociado al vertice que se encuentre en el mismo indice.
+	 * 
+	 * @param color Color RGB
+	 */
 	public void addColor(Vector3 color) {
 		colors.add(color);
 	}
 	
+	/**
+	 * Devuelve el color situado en el indice especificado del Array de colores
+	 * 
+	 * @param index Indice del vertice al que va asociado el color
+	 * @return Color RGB o null si esta {@link Geometry} no contiene informacion de color
+	 */
 	public Vector3 getColor(int index) {
 		if (colors == null) {
 			return null;
@@ -53,10 +97,21 @@ public class Geometry {
 		return colors.get(index);
 	}
 	
+	/**
+	 * Agrega unas coordenadas de textura a la geometria. Las coordenadas UV iran asociadas al vertice que se encuentre en el mismo indice.
+	 * 
+	 * @param textureUV Coordenadas UV de la textura asociada al vertice
+	 */
 	public void addTextureUV(Vector2 textureUV) {
 		texturesUV.add(textureUV);
 	}
 	
+	/**
+	 * Devuelve las coordenadas de textura situadas en el indice especificado del Array de coordenadas UV
+	 * 
+	 * @param index Indice del vertice al que van asociadas las coordenadas UV
+	 * @return Color RGB o null si esta {@link Geometry} no contiene informacion de color
+	 */
 	public Vector2 getTextureUV(int index) {
 		if (texturesUV == null) {
 			return null;
@@ -64,54 +119,112 @@ public class Geometry {
 		return texturesUV.get(index);
 	}
 	
+	/**
+	 * Devuelve si se ha solicitado actualizar los vertices
+	 * 
+	 * @return true si se ha solicitado actualizar, false en caso contrario
+	 */
 	public boolean isUpdateVerticesSet() {
 		return updateVertices;
 	}
 	
+	/**
+	 * Asigna si se necesita actualizar los vertices o no
+	 * 
+	 * @param updateVertices true para actualizar, false en caso contrario
+	 */
 	protected void setUpdateVertices(boolean updateVertices) {
 		this.updateVertices = updateVertices;
 	}
 	
+	/**
+	 * Solicita actualizar los vertices
+	 */
 	public void updateVertices() {
 		updateVertices = true;
 	}
 	
+	/**
+	 * Devuelve si se ha solicitado actualizar las faces
+	 * 
+	 * @return true si se ha solicitado actualizar, false en caso contrario
+	 */
 	public boolean isUpdateFacesSet() {
 		return updateFaces;
 	}
 	
+	/**
+	 * Asigna si se necesita actualizar las faces o no
+	 * 
+	 * @param updateFaces true para actualizar, false en caso contrario
+	 */
 	protected void setUpdateFaces(boolean updateFaces) {
 		this.updateFaces = updateFaces;
 	}
 	
+	/**
+	 * Solicita actualizar las faces
+	 */
 	public void updateFaces() {
 		updateFaces = true;
 	}
 	
+	/**
+	 * Devuelve si se ha solicitado actualizar los colores
+	 * 
+	 * @return true si se ha solicitado actualizar, false en caso contrario
+	 */
 	public boolean isUpdateColorsSet() {
 		return updateColors;
 	}
 	
+	/**
+	 * Asigna si se necesita actualizar los colores o no
+	 * 
+	 * @param updateColors true para actualizar, false en caso contrario
+	 */
 	protected void setUpdateColors(boolean updateColors) {
 		this.updateColors = updateColors;
 	}
 	
+	/**
+	 * Solicita actualizar los colores
+	 */
 	public void updateColors() {
 		updateColors = true;
 	}
 	
+	/**
+	 * Devuelve si se ha solicitado actualizar las coordenadas UV de las texturas
+	 * 
+	 * @return true si se ha solicitado actualizar, false en caso contrario
+	 */
 	public boolean isUpdateTexturesUVSet() {
 		return updateTexturesUV;
 	}
 	
+	/**
+	 * Asigna si se necesita actualizar las coordenadas UV de las texturas o no
+	 * 
+	 * @param updateTexturesUV true para actualizar, false en caso contrario
+	 */
 	protected void setUpdateTexturesUV(boolean updateTexturesUV) {
 		this.updateTexturesUV = updateTexturesUV;
 	}
 	
+	/**
+	 * Solicita actualizar las coordenadas UV de las texturas
+	 */
 	public void updateTexturesUV() {
 		updateTexturesUV = true;
 	}
 	
+	/**
+	 * Builder que se utiliza para construit objetos {@link Geometry}
+	 * 
+	 * @author Miguel Vicente Linares
+	 * 
+	 */
 	public static class GeometryBuilder {
 		
 		private Array<Vector3> vertices;
@@ -123,6 +236,12 @@ public class Geometry {
 		private boolean usesColors;
 		private boolean usesTexturesUV;
 		
+		/**
+		 * Crea un GeometryBuilder
+		 * 
+		 * @param numVertices Numero de vertices de la geometria
+		 * @param numFaces Numero de faces de la geometria
+		 */
 		public GeometryBuilder(int numVertices, int numFaces) {
 			this.numVertices = numVertices;
 			this.numFaces = numFaces;
@@ -130,14 +249,31 @@ public class Geometry {
 			this.usesTexturesUV = false;
 		}
 		
-		public void useColors() {
+		/**
+		 * Indica que la geometria contiene informacion del color de los vertices
+		 * 
+		 * @return Devuelve este GeometryBuilder
+		 */
+		public GeometryBuilder useColors() {
 			this.usesColors = true;
+			return this;
 		}
 		
-		public void useTexturesUV() {
+		/**
+		 * Indica que la geometria contiene informacion de coordenadas de texturas de los vertices
+		 * 
+		 * @return GeometryBuilder
+		 */
+		public GeometryBuilder useTexturesUV() {
 			this.usesTexturesUV = true;
+			return this;
 		}
 		
+		/**
+		 * Construye el objeto {@link Geometry} con los parametros especificados en el {@link GeometryBuilder}
+		 * 
+		 * @return {@link Geometry}
+		 */
 		public Geometry build() {
 			this.vertices = new Array<Vector3>(numVertices);
 			this.faces = new Array<Vector3>(numFaces);
