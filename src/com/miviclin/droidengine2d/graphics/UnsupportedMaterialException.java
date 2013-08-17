4bc21db532ed090dfbb4ac36a09b9f0d81950f80
Miguel Vicente Linares <miviclin@gmail.com>
2013-08-17 20:53:19 +0200

    Add javadoc to UnsupportedMaterialException

--- src/com/miviclin/droidengine2d/graphics/UnsupportedMaterialException.java
+++ src/com/miviclin/droidengine2d/graphics/UnsupportedMaterialException.java
@@ -1,5 +1,11 @@
 package com.miviclin.droidengine2d.graphics;
 
+/**
+ * Excepcion que se lanza cuando el Material que se pretende utilizar no esta registrado en el objeto Graphics del engine
+ * 
+ * @author Miguel Vicente Linares
+ * 
+ */
 @SuppressWarnings("serial")
 public class UnsupportedMaterialException extends RuntimeException {
 	
