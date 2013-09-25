e4ac22b14f1c48d78ccbae6088155e1bcf267a69
Miguel Vicente Linares <miviclin@gmail.com>
2013-09-25 15:09:19 +0200

    Remove ShaderUtilities and move createProgram(...) to ShaderProgram

--- src/com/miviclin/droidengine2d/graphics/shader/ShaderProgram.java
+++ src/com/miviclin/droidengine2d/graphics/shader/ShaderProgram.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import android.opengl.GLES20;
+import android.util.Log;
 
 import com.miviclin.droidengine2d.graphics.GLDebugger;
 
@@ -158,7 +159,7 @@ public class ShaderProgram {
 			throw new ShaderProgramException("The source code of the shaders is not loaded.\n" +
 					"Ensure setShaders(...) has been called before trying to compile the shader program.");
 		}
-		int programId = ShaderUtilities.createProgram(vertexShaderSource, fragmentShaderSource);
+		int programId = ShaderProgram.createProgram(vertexShaderSource, fragmentShaderSource);
 		if (programId == 0) {
 			return;
 		}
@@ -357,6 +358,67 @@ public class ShaderProgram {
 		int uniformLocation = getUniformLocation(uniformName);
 		GLES20.glUniformMatrix4fv(uniformLocation, numMatrices, false, data, dataOffset);
 		GLDebugger.getInstance().passiveCheckGLError();
+	}
+	
+	/**
+	 * Crea un programa GLSL a partir de los 2 shaders especificados
+	 * 
+	 * @param vertexSource Codigo GLSL del vertex shader
+	 * @param fragmentSource Codigo GLSL del fragment shader
+	 * @return Devuelve el identificador que asigna OpenGL ES 2.0 al programa compilado. Si devuelve 0 es que se ha producido algun error.
+	 */
+	protected static int createProgram(String vertexSource, String fragmentSource) {
+		int vertexShader, fragmentShader, program;
+		
+		vertexShader = compileShader(GLES20.GL_VERTEX_SHADER, vertexSource);
+		if (vertexShader == 0) {
+			return 0;
+		}
+		fragmentShader = compileShader(GLES20.GL_FRAGMENT_SHADER, fragmentSource);
+		if (fragmentShader == 0) {
+			return 0;
+		}
+		program = GLES20.glCreateProgram();
+		if (program != 0) {
+			GLES20.glAttachShader(program, vertexShader);
+			GLDebugger.getInstance().passiveCheckGLError();
+			GLES20.glAttachShader(program, fragmentShader);
+			GLDebugger.getInstance().passiveCheckGLError();
+			GLES20.glLinkProgram(program);
+			int[] linkStatus = new int[1];
+			GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, linkStatus, 0);
+			if (linkStatus[0] != GLES20.GL_TRUE) {
+				Log.e(ShaderProgram.class.getSimpleName(), "Could not link program: ");
+				Log.e(ShaderProgram.class.getSimpleName(), GLES20.glGetProgramInfoLog(program));
+				GLES20.glDeleteProgram(program);
+				program = 0;
+			}
+		}
+		return program;
+	}
+	
+	/**
+	 * Compila el shader.
+	 * 
+	 * @param shaderType Tipo de shader. Utilizar {@link GLES20#GL_VERTEX_SHADER } o {@link GLES20#GL_FRAGMENT_SHADER }
+	 * @param source Codigo GLSL del shader a compilar
+	 * @return Devuelve el identificador que asigna OpenGL ES 2.0 al shader compilado
+	 */
+	private static int compileShader(int shaderType, String source) {
+		int shader = GLES20.glCreateShader(shaderType);
+		if (shader != 0) {
+			GLES20.glShaderSource(shader, source);
+			GLES20.glCompileShader(shader);
+			int[] compiled = new int[1];
+			GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0);
+			if (compiled[0] == 0) {
+				Log.e(ShaderProgram.class.getSimpleName(), "Could not compile shader " + shaderType + ":");
+				Log.e(ShaderProgram.class.getSimpleName(), GLES20.glGetShaderInfoLog(shader));
+				GLES20.glDeleteShader(shader);
+				shader = 0;
+			}
+		}
+		return shader;
 	}
 	
 	@Override
